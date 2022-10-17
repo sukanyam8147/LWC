2ddfc791e0bb619d56a8323702a0c064d9cddafc
Sukanya Maddipatla <sukanya.maddipatla@wisseninfotech.com>
2022-10-17 13:18:56 +0530

    Null Vs Undefined and Comparision

---
+++ NVsU.js
@@ -0,0 +1,18 @@
+//Normal Comparision
+//it is compare the values but not data types
+console.log(2=='2')
+//output is true
+//it is compare the values and data types
+console.log(2==='2')
+//output is false
+//Null vs Undefined
+var u
+console.log(u)
+var n=null
+console.log(n)
+//it is compare the values but not data types
+console.log(u==n)
+//output is true
+//it is compare the values and data types
+console.log(u===n)
+//output is false
