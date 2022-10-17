d17cc8e7b64b9b5d66062bb3d7fcbf781898c97c
Sukanya Maddipatla <sukanya.maddipatla@wisseninfotech.com>
2022-10-17 13:00:49 +0530

    Data Types

---
+++ DataTypes.js
@@ -0,0 +1,24 @@
+//string
+var s='ammu'
+console.log(typeof s)
+//number 
+var n=10
+console.log(typeof n)
+//big number
+var x=1090n
+console.log(typeof x)
+//undefined
+var y
+console.log(typeof y)
+//null
+var x=null
+console.log(typeof x)
+//object
+var obj={}
+console.log(typeof obj)
+//array
+var arr=[]
+console.log(typeof arr)
+//symbol
+var sym=Symbol('Id')
+console.log(typeof sym)
